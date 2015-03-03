00376aa9791b6e497a2ab0605c327e1274d39924
Brandon Mrsny <bmrsny@gmail.com>
2015-03-03 13:41:47 -0700

    Twitter keys added in production and development

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,9 @@
 class SessionsController < ApplicationController
   def create
-  	 user = User.find_or_create_from_auth((auth))
+  	 user = User.find_or_create_from_auth(auth)
      if user
        session[:user_id] = user.id
-       redirect_to dashboard_path
+       redirect_to root_path
      else
        redirect_to rooth_path
      end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,4 @@
 class WelcomeController < ApplicationController
 	def index
-		flash[:success] = "Whooo You Did It Bruh!"
 	end
 end
