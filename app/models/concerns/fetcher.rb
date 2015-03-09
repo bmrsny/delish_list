f46b7676ade66f16832ce5097c73aefd714a1b4e
Brandon Mrsny <bmrsny@gmail.com>
2015-03-08 23:12:33 -0600

    can search the api and find by recipe id needs some tweaking to fit the project

--- app/models/concerns/fetcher.rb
+++ app/models/concerns/fetcher.rb
@@ -14,7 +14,8 @@ class Fetcher
 		self.class.get("/recipe/#{id.to_s}?api_key=#{ENV["BIG_OVEN_KEY"]}", @options)
 	end
 
-	def users
-		self.class.get("/2.2/users", @options)
+	def search_input#(keyword) 
+		self.class.get("/recipes?title_kw=oyster&pg=1&rpp=20&api_key=#{ENV['BIG_OVEN_KEY']}", @options)
+		#recipes?title_kw=oysters&pg=1&rpp=20&api_key={your-api-key}
 	end
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -5,8 +5,35 @@ class Recipe < ActiveRecord::Base
 	has_many :recipe_ingredients
 	has_many :ingredients, through: :recipe_ingredients
 
+	def self.search_by_keyword#(keyword)
+		conn = Fetcher.new()
+		response = conn.search_input#(keyword)
+		recipe_info = response.parsed_response["RecipeSearchResult"]["Results"]["RecipeInfo"]
+		recipe_info.map do |recipe_hash|
+			recipe_hash["Title"]
+		end	
+	end
+
 	def self.fetch_single(id)
 		conn = Fetcher.new()
 		response = conn.single_recipe(id)
+		#response.parsed_response["Recipe"]["Title"]
 	end
+
+	def self.get_title(response)
+		response.parsed_response["Recipe"]["Title"]
+	end
+
+	def self.get_ingredients(response)
+		response.parsed_response["Recipe"][""]
+	end
+
+	def self.get_ingredient_names(response)
+		response["Recipe"]["Ingredients"]["Ingredient"].map do |ingredient|
+			ingredient["Name"]
+		end
+	end
+
+	#def self.find_or_create_from_big_oven(response)
+	#end
 end
