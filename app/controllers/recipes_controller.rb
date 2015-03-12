6b6db69c09cdc6ba0fdedd4692db991a35cc74b0
Brandon Mrsny <bmrsny@gmail.com>
2015-03-11 21:46:02 -0600

    user can add ingredients to a new shopping list or add to an existing shopping list

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,13 +1,5 @@
 class RecipesController < ApplicationController
 	def index
-		#if Fetcher.new.search_by_keyword(params[:search]) == false
-		#	flash[:error] = "#{params[:search]} is not a valid search term."
-		#	redirect_to root_path
-		#else
-		#Fetcher.new.create_or_find_recipes(params[:search])
-		#	#@recipes = Recipe.all.last(20)
-		#	@recipes = Recipe.order("created_at desc").limit(20)
-		#end
 		if Recipe.search(params[:search]) == false
 			flash[:alert] = "#{params[:search]} is not a valid search term."
 			redirect_to root_path
